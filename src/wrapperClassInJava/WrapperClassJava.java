9257465d96b7c011deeea32d64adcc6ef127fb63
harshtrivedi312 <bluefishartandanimation@gmail.com>
2023-07-12 10:59:00 -0400

    block Wrapper Classes

--- src/wrapperClassInJava/WrapperClassJava.java
+++ src/wrapperClassInJava/WrapperClassJava.java
@@ -1,67 +1,67 @@
-package wrapperClassInJava;
-
-public class WrapperClassJava {
-
-    // Integer
-
-    //The Integer class is used to wrap the int premetive type.
-    //it provvides methods to convert int values to String, parse
-    //values to obtain int, perform match operations and more..
-
-
-
-
-    public static void main(String[] args) {
-
-        int number = 42;
-        Integer wrapperNumber = Integer.valueOf(number); //wrapping int value
-        String numberAsString = wrapperNumber.toString();
-        int parseNumber = Integer.parseInt("42");
-
-        System.out.println(number);
-        System.out.println(wrapperNumber);
-        System.out.println(numberAsString);
-        System.out.println(parseNumber);
-
-        boolean flag = true;
-        Boolean wrapperFlag = Boolean.valueOf(flag);    // Wrapping a boolean value
-        String flagAsString = wrapperFlag.toString();   // Converting Boolean to String
-        boolean parsedFlag = Boolean.parseBoolean("true");  // Parsing a String to obtain a boolean
-
-        char letter = 'A';
-        Character wrapperLetter = Character.valueOf(letter);  // Wrapping a char value
-        String letterAsString = wrapperLetter.toString();     // Converting Character to String
-        boolean isDigit = Character.isDigit('5');              // Checking if a character is a digit
-
-
-        int result = fibonacci(10);  // Calculates the 6th Fibonacci number
-        System.out.println(result);  // Output: 8
-
-    }
-
-    // JAVA Recursion -- a method calling itself infinite
-    //  The Fibonacci series is a sequence of numbers where each number is the sum
-    //  of the two preceding ones.
-    public static int fibonacci(int n) {
-        if (n == 0 || n == 1) {
-            return n;  // Base case: Fibonacci number is equal to n
-        } else {
-            return fibonacci(n - 1) + fibonacci(n - 2);  // Recursive call
-        }
-
-    }
-
-//    0+1=1
-//    1+1=2
-//    2+1=3
-//    3+2=5
-//    5+3=8
-//    8+5=13
-//    13+8=21
-//    21+13=34
-
-
-
-
-
-}
+//package wrapperClassInJava;
+//
+//public class WrapperClassJava {
+//
+//    // Integer
+//
+//    //The Integer class is used to wrap the int premetive type.
+//    //it provvides methods to convert int values to String, parse
+//    //values to obtain int, perform match operations and more..
+//
+//
+//
+//
+//    public static void main(String[] args) {
+//
+//        int number = 42;
+//        Integer wrapperNumber = Integer.valueOf(number); //wrapping int value
+//        String numberAsString = wrapperNumber.toString();
+//        int parseNumber = Integer.parseInt("42");
+//
+//        System.out.println(number);
+//        System.out.println(wrapperNumber);
+//        System.out.println(numberAsString);
+//        System.out.println(parseNumber);
+//
+//        boolean flag = true;
+//        Boolean wrapperFlag = Boolean.valueOf(flag);    // Wrapping a boolean value
+//        String flagAsString = wrapperFlag.toString();   // Converting Boolean to String
+//        boolean parsedFlag = Boolean.parseBoolean("true");  // Parsing a String to obtain a boolean
+//
+//        char letter = 'A';
+//        Character wrapperLetter = Character.valueOf(letter);  // Wrapping a char value
+//        String letterAsString = wrapperLetter.toString();     // Converting Character to String
+//        boolean isDigit = Character.isDigit('5');              // Checking if a character is a digit
+//
+//
+//        int result = fibonacci(10);  // Calculates the 6th Fibonacci number
+//        System.out.println(result);  // Output: 8
+//
+//    }
+//
+//    // JAVA Recursion -- a method calling itself infinite
+//    //  The Fibonacci series is a sequence of numbers where each number is the sum
+//    //  of the two preceding ones.
+//    public static int fibonacci(int n) {
+//        if (n == 0 || n == 1) {
+//            return n;  // Base case: Fibonacci number is equal to n
+//        } else {
+//            return fibonacci(n - 1) + fibonacci(n - 2);  // Recursive call
+//        }
+//
+//    }
+//
+////    0+1=1
+////    1+1=2
+////    2+1=3
+////    3+2=5
+////    5+3=8
+////    8+5=13
+////    13+8=21
+////    21+13=34
+//
+//
+//
+//
+//
+//}
